5bac621ee0180bfc6e24bb998bba6be5c7a7822e
Marco Beinbrech <marco.beinbrech@fotograf.de>
2015-08-05 16:13:08 +0200

    added testMode param

--- src/Gateway.php
+++ src/Gateway.php
@@ -24,6 +24,7 @@ class Gateway extends AbstractGateway
         return array(
             'secretApiKey' => '',
             'publicApiKey' => '',
+            'testMode' => false,
         );
     }
 
